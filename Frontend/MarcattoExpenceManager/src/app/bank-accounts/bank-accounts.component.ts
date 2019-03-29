f7654b519ed3fc49dc189f4e92ab60e14e788d26
Fagun <mdabuzaforfagun@gmail.com>
2019-03-29 23:38:46 +0600

    add back button to bank page

--- Frontend/MarcattoExpenceManager/src/app/bank-accounts/bank-accounts.component.ts
+++ Frontend/MarcattoExpenceManager/src/app/bank-accounts/bank-accounts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bank-accounts',
@@ -9,9 +10,12 @@ export class BankAccountsComponent implements OnInit {
 
   bankName: string;
   accountNumber: string;
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  btnBackClick() {
+    this.router.navigateByUrl("dashbaord");
+  }
 }
